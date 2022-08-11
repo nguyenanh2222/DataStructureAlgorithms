21f992d21dea2a6a796652fa3c432ec0cefbdd7e
anh <nguyenvieanh2103@gmail.com>
2022-08-11 18:07:04 +0700

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	graph_theory.py

--- connected _graph.py
+++ connected _graph.py
@@ -1,62 +1,34 @@
 import numpy as np
-from networkx import connected_components
-from numpy import VisibleDeprecationWarning
+class Graph():
+
+    def read_file(self, file_name):
+        f = open(f'{file_name}', 'r')
+        arr = []
+        for line in f.readlines():
+            arr.append(line.split())
+        return arr
 
 
-class ConnectedGraph():
-    def __init__(self, V):
-        # No. of vertices
-        self.V = V
-        # adjacency list
-        self.adj = [[] for i in range(self.V)]
+    def check_graph(self, file_name):
+        a = 0
+        data = self.read_file(file_name=file_name)
+        A = np.array(data)
+        for i in range(6):
+            for j in range(5):
+                if A[i][i] != 0:
+                    a = 3
+                elif A[i][i] == A[j][i]:
+                    a = 0
+                elif A[i][i] == A[j][i]:
+                    a = 1
+                    break
+        if a == 1:
+            print("Day la do thi vo huong")
+        elif a == 0:
+            print("Day la do thi co huong")
+        elif a == 3:
+            print("Do thi khong hop le")
 
-    def number_of_connect_components(self):
-        visited = [False for i in range(self.V)]
-        count = 0
-        for v in range(self.V):
-            if (visited[v] == False):
-                self.dfs_utility(v, visited)
-                count += 1
-        return count
-
-    def dfs_utility(self, temp, v, visited):
-        visited[v] = True
-        # recur for all vertices
-        # adjecent to this vertex
-        temp.append(v)
-        for i in self.adj[v]:
-            if visited[i] == False:
-                temp = self.dfs_utility(temp, i, visited)
-        return temp
-
-    # add an undirected edge
-    def add_edge(self, v, w):
-        self.adj[v].append(w)
-        self.adj[w].append(v)
-
-    def connect_component(self):
-        visited = []
-        conn_component = []
-        for i in range(self.V):
-            visited.append(False)
-        for v in range(self.V):
-            if visited[v] == False:
-                temp = []
-                conn_component.append(self.dfs_utility(temp, v, visited))
-                return conn_component
-
-
-if __name__ == "__main__":
-    g = ConnectedGraph(9)
-    g.add_edge(5, 3)
-    g.add_edge(3, 7)
-    g.add_edge(3, 0)
-    g.add_edge(0, 4)
-    g.add_edge(4, 8)
-    g.add_edge(2, 0)
-    g.add_edge(6, 1)
-    connected_components = g.connect_component()
-    print(connected_components)
 """
 doc file kiem tra do thi co huong va do thi vo huong
 tao list bang nhan 9 dinh
@@ -67,6 +39,8 @@ for i in range(dinh):
  nhan[i] = sothanhphanlienthong
  -> viet ham de quy tim cac dinh lien thong(do thi, bang nhan, dinh: int):
  for (i, len())..
- 
-
 """
+g=Graph()
+data = g.read_file('connected_graph.txt')
+print(data)
+g.check_graph('connected_graph.txt')
