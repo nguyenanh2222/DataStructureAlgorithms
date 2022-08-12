89f8c0d5a690109620a5539300e4e8b75ef27b6f
anh <nguyenvieanh2103@gmail.com>
2022-08-12 20:38:58 +0700

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	graph_theory.py

--- stack.py
+++ stack.py
@@ -1,30 +1,30 @@
-class Node:
+class Node():
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-class Stack:
+class Stack():
     def __init__(self):
         self.head = Node("head")
         self.size = 0
 
     def __str__(self):
-        temp = self.head.next
-        temp_str = ""
-        while temp:
-            temp_str += f"{temp.value}-->"
-            temp = temp.next
-        return temp_str
+        cur = self.head.next
+        out = ""
+        while cur:
+            out += str(cur.value) + "->"
+            cur = cur.next
+        return out[:-3]
 
     def getSize(self):
         return self.size
 
-    def isEmty(self):
+    def isEmpty(self):
         return self.size == 0
 
     def peek(self):
-        if self.isEmty():
+        if self.isEmpty():
             raise Exception("Peeking from an empty stack")
         return self.head.next.value
 
@@ -35,23 +35,20 @@ class Stack:
         self.size += 1
 
     def pop(self):
-        if self.isEmty():
+        if self.isEmpty():
             raise Exception("Popping from an empty stack")
         remove = self.head.next
         self.head.next = self.head.next.next
         self.size -= 1
         return remove.value
 
-    # delete, print
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     stack = Stack()
     for i in range(1, 11):
         stack.push(i)
-    print(f'Stack: {stack}')
-
+    print(f"Stack: {stack}")
     for i in range(1, 6):
         remove = stack.pop()
-        print(f"Pop: {remove}")
-    print(f"Stack: {stack}")
+    print(f"Stack:{stack}")
+
