8226bb7c1c8b30b6ee39d68d0a3602957dabae4b
anh2222 <nguyenvieanh2103@gmail.com>
2022-10-26 06:58:41 +0700

    add testPython

---
+++ TestPython/test01.py
@@ -0,0 +1,29 @@
+import math
+
+input = "12341112233789"
+
+# chunk = {}
+# for num in input:
+#     if num in chunk:
+#         pass
+#     else:
+#         chunk[num]= input.count(num) // 2
+# print(sum(chunk.values()))
+
+
+
+
+# temp = {}
+# for i in input:
+#     if i in temp:
+#         temp[i] += 1
+#     else :
+#         temp.update(**{i:1})
+#
+# sum=0
+#
+# for i in temp:
+#     sum+= int(temp[i]/2)
+#
+# print(sum)
+
---
+++ TestPython/test05.py
@@ -0,0 +1,4 @@
+def rev(string: str) -> str:
+    return string[::-1]
+
+print(rev("iloveu"))
